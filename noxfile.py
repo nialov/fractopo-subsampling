2d0d0f5353a8bda147c0ab4f1c08b031942a8cbe
nialov <nikolasovaskainen@hotmail.com>
2021-02-17 11:53:31 +0200

    copier done

---
+++ noxfile.py
@@ -0,0 +1,192 @@
+"""
+Nox test suite.
+"""
+import logging
+from pathlib import Path
+from shutil import copy2, copytree, rmtree
+from typing import List
+
+import nox
+
+docs_apidoc_dir_path = Path("docs_src/apidoc")
+docs_dir_path = Path("docs")
+package_name = "fractopo_scripts"
+tests_name = "tests"
+pipfile_lock = "Pipfile.lock"
+notebooks_name = "notebooks"
+docs_notebooks_name = "docs_src/notebooks"
+tasks_name = "tasks.py"
+noxfile_name = "noxfile.py"
+pylama_config = "pylama.ini"
+
+
+docs_notebooks = Path("docs_src/notebooks").glob("*.ipynb")
+regular_notebooks = Path(notebooks_name).glob("*.ipynb")
+all_notebooks = list(docs_notebooks) + list(regular_notebooks)
+
+
+def filter_paths_to_existing(*iterables) -> List[str]:
+    return [path for path in iterables if Path(path).exists()]
+
+
+@nox.session(python="3.8")
+def tests_strict(session: nox.Session):
+    """
+    Run strict test suite.
+    """
+    tmp_dir = session.create_tmp()
+    for to_copy in (package_name, tests_name, pipfile_lock, notebooks_name):
+        if Path(to_copy).is_dir():
+            copytree(to_copy, Path(tmp_dir) / to_copy)
+        elif Path(to_copy).is_file():
+            copy2(to_copy, tmp_dir)
+        elif Path(to_copy).exists():
+            ValueError("File not dir or file.")
+        else:
+            logging.error(f"Expected {to_copy} to exist.")
+    session.chdir(tmp_dir)
+    session.install("pipenv")
+    session.run("pipenv", "--rm", success_codes=[0, 1])
+    session.run(
+        "pipenv",
+        "sync",
+        "--python",
+        f"{session.python}",
+        "--dev",
+        "--bare",
+    )
+    session.run(
+        "pipenv",
+        "run",
+        "coverage",
+        "run",
+        "--include",
+        f"{package_name}/**.py",
+        "-m",
+        "pytest",
+    )
+    session.run("pipenv", "run", "coverage", "report", "--fail-under", "70")
+
+
+@nox.session(python="3.8")
+def tests_lazy(session):
+    """
+    Run lazy test suite.
+    """
+    session.install(".[dev]")
+    # Test with pytest
+    session.run("pytest")
+    # Test notebook(s)
+    for notebook in all_notebooks:
+        session.run("ipython", str(notebook))
+
+
+@nox.session(python="3.8")
+def format(session):
+    """
+    Format python files, notebooks and docs_src.
+    """
+    session.install("black", "black-nb", "isort")
+    existing_paths = filter_paths_to_existing(
+        package_name, tests_name, tasks_name, noxfile_name
+    )
+    # Format python files
+    session.run("black", *existing_paths)
+    # Format python file imports
+    session.run(
+        "isort",
+        *existing_paths,
+    )
+    # Format notebooks
+    for notebook in all_notebooks:
+        session.run("black-nb", notebook)
+
+
+@nox.session(python="3.8")
+def lint(session):
+    """
+    Lint python files, notebooks and docs_src.
+    """
+    session.install("rstcheck", "sphinx", "black", "black-nb", "isort", "pylama")
+    existing_paths = filter_paths_to_existing(
+        package_name, tests_name, tasks_name, noxfile_name
+    )
+    # Lint docs
+    session.run(
+        "rstcheck",
+        "-r",
+        "docs_src",
+        "--ignore-directives",
+        "automodule",
+    )
+    # Lint python files with black (all should be formatted.)
+    session.run("black", "--check", *existing_paths)
+    session.run(
+        "isort",
+        "--check-only",
+        *existing_paths,
+    )
+    # Lint with pylama
+    session.run(
+        "pylama",
+        "-o",
+        pylama_config,
+        *existing_paths,
+    )
+
+    for notebook in all_notebooks:
+        # Lint notebooks with black-nb (all should be formatted.)
+        session.run("black-nb", "--check", notebook)
+
+
+@nox.session
+def requirements(session):
+    """
+    Sync Pipfile to setup.py with pipenv-setup.
+    """
+    session.install("pipenv-setup")
+    session.run("pipenv-setup", "sync", "--pipfile", "--dev")
+
+
+@nox.session
+def docs(session):
+    """
+    Make documentation.
+
+    Installation mimics readthedocs install.
+    """
+    # Install with setup.py[dev] installation
+    session.install(".[dev]")
+
+    # Remove old apidocs
+    if docs_apidoc_dir_path.exists():
+        rmtree(docs_apidoc_dir_path)
+
+    # Remove all old docs
+    if docs_dir_path.exists():
+        rmtree(docs_dir_path)
+
+    # Execute and fill cells in docs notebooks
+    for notebook in docs_notebooks:
+        session.run(
+            "jupyter",
+            "nbconvert",
+            "--to",
+            "notebook",
+            "--inplace",
+            "--execute",
+            str(notebook),
+        )
+    # Create apidocs
+    session.run(
+        "sphinx-apidoc", "-o", "./docs_src/apidoc", f"./{package_name}", "-e", "-f"
+    )
+
+    # Create docs in ./docs folder
+    session.run(
+        "sphinx-build",
+        "./docs_src",
+        "./docs",
+        "-b",
+        "html",
+    )
---
+++ tasks.py
@@ -0,0 +1,104 @@
+"""
+Invoke tasks.
+
+Most tasks employ nox to create a virtual session for testing.
+"""
+from invoke import task
+from invoke import UnexpectedExit
+
+nox_parallel_sessions = (
+    "tests_strict",
+    "tests_lazy",
+)
+
+package_name = "fractopo_scripts"
+
+
+@task
+def requirements(c):
+    """
+    Sync requirements from Pipfile to setup.py.
+    """
+    c.run("nox --session requirements")
+
+
+@task
+def format(c):
+    """
+    Format everything.
+    """
+    c.run("nox --session format")
+
+
+@task(pre=[format])
+def lint(c):
+    """
+    Lint everything.
+    """
+    c.run("nox --session lint")
+
+
+@task
+def nox_parallel(c):
+    """
+    Run selected nox test suite sessions in parallel.
+    """
+    # Run asynchronously and collect promises
+    print(f"Running {len(nox_parallel_sessions)} nox test sessions.")
+    promises = [
+        c.run(
+            f"nox --session {nox_test} --no-color",
+            asynchronous=True,
+            timeout=360,
+        )
+        for nox_test in nox_parallel_sessions
+    ]
+
+    # Join all promises
+    results = [promise.join() for promise in promises]
+
+    # Check if Result has non-zero exit code (should've already thrown error.)
+    for result in results:
+        if result.exited != 0:
+            raise UnexpectedExit(result)
+
+    # Report to user of success.
+    print(f"{len(results)} nox sessions ran succesfully.")
+
+
+@task
+def pytest(c):
+    """
+    Run tests with pytest in currently installed environment.
+
+    Much faster than full `test` suite.
+    """
+    c.run(f"coverage run --include '{package_name}/**.py' -m pytest")
+    c.run("coverage report --fail-under 70")
+
+
+@task(pre=[pytest, nox_parallel])
+def test(_):
+    """
+    Run tests.
+
+    This is an extensive suite. It first tests in current environment and then
+    creates virtual sessions with nox to test installation -> tests.
+    """
+
+
+@task(pre=[requirements])
+def docs(c):
+    """
+    Make documentation to docs using nox.
+    """
+    c.run("nox --session docs")
+
+
+@task(pre=[test, lint, docs])
+def make(_):
+    """
+    Make all.
+    """
+    print("---------------")
+    print("make succesful.")
