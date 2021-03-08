953d4f9657b8ecf25690cef6464967bf4de0c639
nialov <nikolasovaskainen@hotmail.com>
2021-03-08 19:14:27 +0200

    move from trace_repo

--- fractopo_scripts/simulation/utils.py
+++ fractopo_scripts/simulation/utils.py
@@ -1,3 +1,199 @@
 """
 General utilities for simulation.
 """
+import random
+import numpy as np
+from pandas.core.groupby.generic import DataFrameGroupBy
+from pathlib import Path
+from typing import List, Dict, Union, Tuple, Sequence, Literal
+import pandas as pd
+import geopandas as gpd
+from matplotlib.axes import Axes
+
+
+class Utils:
+
+    """
+    General selection, column names, etc. data.
+    """
+
+    circle_names_with_diameter = {
+        "Getaberget_20m_4_3_area": 50,
+        "Getaberget_20m_9_2_area": 50,
+        "Getaberget_20m_8_3_area": 50,
+        "Getaberget_20m_7_1_area": 50,
+        "Getaberget_20m_7_2_area": 20,  # 20 m
+        "Getaberget_20m_5_1_area": 50,
+        "Getaberget_20m_2_1_area": 40,  # 40 m
+        "Getaberget_20m_2_2_area": 50,
+        "Getaberget_20m_1_1_area": 50,
+        "Getaberget_20m_1_2_area": 40,  # 40 m
+        "Getaberget_20m_1_3_area": 10,  # 10 m
+        "Getaberget_20m_1_4_area": 50,
+        "Havsvidden_20m_1_area": 50,
+        "Segelskar_20m_1_1_area": 40,  # 40 m
+    }
+
+    radius = "radius"
+    relative_coverage = "relative coverage"
+    name = "name"
+    trace_power_law_vs_lognormal_r = "trace power_law vs. lognormal R"
+    trace_power_law_vs_lognormal_p = "trace power_law vs. lognormal p"
+
+    params_with_func = {
+        "Fracture Intensity (Mauldon)": "mean",
+        "Connections per Branch": "mean",
+        "trace power_law exponent": "mean",
+        "branch power_law exponent": "mean",
+        "Fracture Intensity P21": "mean",
+        "Number of Traces": "sum",
+        "Number of Branches": "sum",
+        "radius": "sum",
+    }
+
+    base_circle_ids_csv_path = Path("../results/base_circle_ids.csv")
+    base_circle_reference_value_csv_path = Path("../results/base_reference_values.csv")
+
+
+def random_sample_of_circles(
+    grouped: DataFrameGroupBy, circle_names_with_diameter: Dict[str, int]
+) -> List[pd.Series]:
+    """
+    Get a random sample of circles from grouped simulation data.
+
+    Both the amount of overall circles and which circles within each group
+    is random. Data is grouped by target area name.
+    """
+    names = list(grouped.groups.keys())
+    areas = [np.pi * (circle_names_with_diameter[name] / 2) ** 2 for name in names]
+    idxs = list(range(0, len(grouped)))
+    how_many = random.randint(1, len(grouped))
+
+    which_idxs = []
+    for _ in range(how_many):
+        choice = random.choices(population=idxs, weights=areas, k=1)[0]
+        while choice in which_idxs:
+            choice = random.choices(population=idxs, weights=areas, k=1)[0]
+        which_idxs.append(choice)
+
+    # which = []
+    # for _ in range(how_many):
+    #     which_idx = random.randint(0, len(grouped) - 1)
+    #     # Do not choose duplicates and do base circle area weighting
+    #     while which_idx in which or not apply_area_weight(
+    #         names[which_idx], circle_names_with_diameter
+    #     ):
+    #         which_idx = random.randint(0, len(grouped) - 1)
+    #     which.append(which_idx)
+
+    assert len(which_idxs) == how_many
+
+    chosen: List[pd.Series] = []
+
+    for idx, (_, group) in enumerate(grouped):
+        if idx not in which_idxs:
+            continue
+        which_circle = random.randint(0, group.shape[0] - 1)
+        chosen.append(group.iloc[which_circle])
+
+    assert len(chosen) == how_many
+    return chosen
+
+
+def numpy_to_python_type(value):
+    """
+    Convert to Python type from numpy with .item().
+    """
+    try:
+        return value.item()
+    except AttributeError:
+        return value
+
+
+def aggregate_chosen(
+    chosen: List[pd.Series], params_with_func: Dict[str, Literal["mean", "sum"]]
+) -> Dict[str, Union[float, int]]:
+    """
+    Aggregate a collection of simulation circles for params.
+
+    Weights averages by the area of each simulation circle.
+    """
+    total_area = numpy_to_python_type(sum([srs["area"] for srs in chosen]))
+    assert isinstance(total_area, (float, int))
+    total_count = len(chosen)
+    values = dict(area=total_area, circle_count=total_count)
+    for param, func in params_with_func.items():
+        if func == "mean":
+            func_value = np.average(
+                [srs[param] for srs in chosen], weights=[srs["area"] for srs in chosen]
+            )
+        elif func == "sum":
+            func_value = np.array([srs[param] for srs in chosen]).sum()
+        else:
+            raise ValueError("Expected mean or sum.")
+        func_value = numpy_to_python_type(func_value)
+        assert isinstance(func_value, (float, int))
+        values[param] = func_value
+    return values
+
+
+def constrain_radius(
+    names: np.ndarray, radiuses: np.ndarray, circle_names_with_diameter: Dict[str, int]
+) -> List[bool]:
+    """
+    Constrain dataset radiuses to one fourth of the full diameter.
+    """
+    constrained = []
+    for name, radius in zip(names, radiuses):
+        assert name in circle_names_with_diameter
+        full_radius = circle_names_with_diameter[name]
+        constrained.append(full_radius / 4 >= radius)
+    assert len(constrained) == len(names) == len(radiuses)
+    return constrained
+
+
+def filter_dataframe(
+    df: Union[gpd.GeoDataFrame, pd.DataFrame],
+    filter_names: List[str],
+    filter_radius: Tuple[float, float],
+    relative_coverage_threshold: float,
+) -> Union[gpd.GeoDataFrame, pd.DataFrame]:
+    """
+    Filter (Geo)DataFrame to input names and thresholds.
+    """
+    start_length = df.shape[0]
+    assert df.shape[0] > 0
+    named_df = df.loc[np.isin(df[Utils.name], filter_names)]
+    print(f"Name filtered: {start_length - named_df.shape[0]}")
+    assert named_df.shape[0] > 0
+    radius_df = named_df.loc[
+        [filter_radius[0] <= val <= filter_radius[1] for val in named_df[Utils.radius]]
+    ]
+    print(f"Radius filtered: {named_df.shape[0] - radius_df.shape[0] }")
+    assert radius_df.shape[0] > 0
+    coverage_df = radius_df.loc[
+        radius_df[Utils.relative_coverage] < relative_coverage_threshold
+    ]
+    print(f"Coverage filtered: {radius_df.shape[0] - coverage_df.shape[0] }")
+    assert coverage_df.shape[0] > 0
+    return coverage_df
+
+
+def paper_figsize(
+    multiplier: float,
+    paper_height=11.7,
+    paper_width=8.27,
+):
+    """
+    Get figsize for A4.
+    """
+    return (paper_height, min([paper_width, paper_width * multiplier]))
+
+
+def label_point(
+    xs: Sequence[float], ys: Sequence[float], vals: Sequence, ax: Axes, **text_kwargs
+):
+    """
+    Label points in plot.
+    """
+    [ax.text(x + 0.02, y, str(val), **text_kwargs) for x, y, val in zip(xs, ys, vals)]
---
+++ tests/simulations/test_utils.py
@@ -0,0 +1,81 @@
+"""
+Tests for fractopo_scripts.simulation.utils.
+"""
+import tests
+import pytest
+import pandas as pd
+import numpy as np
+import fractopo_scripts.simulation.utils as utils
+
+
+def test_random_sample_of_circles():
+    """
+    Test random_sample_of_circles.
+    """
+    df = pd.DataFrame(
+        [
+            {"key": "a", "value": 1},
+            {"key": "a", "value": 1},
+            {"key": "b", "value": 2},
+            {"key": "b", "value": 2},
+            {"key": "b", "value": 2},
+            {"key": "b", "value": 2},
+            {"key": "b", "value": 2},
+            {"key": "b", "value": 2},
+            {"key": "b", "value": 1000},
+        ]
+    ).astype({"key": "category"})
+    grouped = df.groupby(by="key")
+    circle_names_with_diameter = {"a": 50, "b": 1}
+    single_result = utils.random_sample_of_circles(grouped, circle_names_with_diameter)
+
+    assert isinstance(single_result, list)
+    assert all([isinstance(val, pd.Series) for val in single_result])
+
+    results = [
+        utils.random_sample_of_circles(grouped, circle_names_with_diameter)
+        for _ in range(100)
+    ]
+    name_counts = dict()
+    for result in results:
+        for srs in result:
+            srs_key = srs["key"]
+            name_counts[srs_key] = (
+                1 if srs_key not in name_counts else name_counts[srs_key] + 1
+            )
+    assert name_counts["a"] > name_counts["b"]
+
+
+def test_aggregate_chosen_manual():
+    """
+    Test aggregate_chosen.
+    """
+    chosen_dicts = [
+        {"area": 1, "intensity": 5},
+        {"area": 10, "intensity": 5},
+    ]
+    chosen = [pd.Series(cd) for cd in chosen_dicts]
+    params_with_func = {"intensity": "mean"}
+    result = utils.aggregate_chosen(chosen, params_with_func)
+    assert isinstance(result, dict)
+    assert all([isinstance(val, str) for val in result])
+    assert all([isinstance(val, (float, int)) for val in result.values()])
+    assert np.isclose(result["intensity"], 5)
+
+
+@pytest.mark.parametrize(
+    "chosen,params_with_func,assume_result", tests.test_aggregate_chosen_params()
+)
+def test_aggregate_chosen(chosen, params_with_func, assume_result):
+    """
+    Test aggregate_chosen with pytest params.
+    """
+    result = utils.aggregate_chosen(chosen, params_with_func)
+    assert isinstance(result, dict)
+    for key in result:
+        if key not in assume_result:
+            continue
+        assert np.isclose(result[key], assume_result[key])
+
+
+# Hello
